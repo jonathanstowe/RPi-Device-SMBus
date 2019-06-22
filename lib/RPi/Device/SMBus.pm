3297e30cd6f282c9498d046ea33333a360dd52a9
Jonathan Stowe <jns@gellyfish.co.uk>
2019-06-22 10:04:02 +0100

    Rename the exception classes
    The X::IO class was causing a namespace collision with the similarly
    named class in the rakudo core.
    
    This came up in https://github.com/rakudo/rakudo/issues/3011

--- lib/RPi/Device/SMBus.pm
+++ lib/RPi/Device/SMBus.pm
@@ -26,11 +26,14 @@ RPi::Device::SMBus - SMBus/i²c interface for Raspberry Pi
 
 =head1 DESCRIPTION
 
-This is an SMBus/i²c interface that has been written and tested for the Raspberry Pi, however it uses a fairly generic
-POSIX interface so if your platform exposes the i²c interface as a character special device it may work.
+This is an SMBus/i²c interface that has been written and tested for
+the Raspberry Pi, however it uses a fairly generic POSIX interface so if
+your platform exposes the i²c interface as a character special device
+it may work.
 
-In order to use this you will need to install and configure the i2c-dev kernel module and tools.  On a default Debian
-image you should be able to just do:
+In order to use this you will need to install and configure the i2c-dev
+kernel module and tools.  On a default Debian image you should be able
+to just do:
 
     sudo apt-get install libi2c-dev i2c-tools
 
@@ -47,39 +50,48 @@ You can determine the bus address of your device by doing:
 
     sudo i2cdetect -y 1  # replace the 1 with a 0 for older versions
 
-(Obviously the device should be connected, consult the manual for your device about this.)
+(Obviously the device should be connected, consult the manual for your
+device about this.)
 
-Which should give you the hexadecimal address of your device.  Some devices may not respond, so you may want to either check
-the data sheet of your device or read the C<i2cdetect> manual page to get other options.
+Which should give you the hexadecimal address of your device.  Some
+devices may not respond, so you may want to either check the data sheet
+of your device or read the C<i2cdetect> manual page to get other options.
 
 =head1 METHODS
 
-Not all devices will necessarily support all of the methods, here.  You should consult the data sheet for your device to
-determine which commands (passed as the C<Command $command> - an unsigned 8 bit integer) should be used as they will differ
-for all devices.
+Not all devices will necessarily support all of the methods, here.
+You should consult the data sheet for your device to determine which
+commands (passed as the C<Command $command> - an unsigned 8 bit integer)
+should be used as they will differ for all devices.
 
 =head2 method new
 
     method new(:$device!, :$address!) returns RPi::Device::SMBus
 
-This is the constructor of the class. The named parameters are both required: C<device> is an the full path (e.g C</dev/i2c-1>) 
-of the character special device that represents your i2c bus, and C<address> should be the 7 bit numeric address (i.e. an integer
-less that 128) of the device on the bus.
+This is the constructor of the class. The named parameters are both
+required: C<device> is an the full path (e.g C</dev/i2c-1>) of the
+character special device that represents your i2c bus, and C<address>
+should be the 7 bit numeric address (i.e. an integer less that 128)
+of the device on the bus.
 
-An exception may be thrown if the device cannot be opened, or it does not support the required ioctls that an i2c device should.
+An exception may be thrown if the device cannot be opened, or it does
+not support the required ioctls that an i2c device should.
 
 =head2 method write-quick
 
     method write-quick(Byte $value) returns Int
 
-This sends a single bit to the device, at the place of the Read/Write bit returning an integer to indicate the result
+This sends a single bit to the device, at the place of the Read/Write
+bit returning an integer to indicate the result
 
 =head2 method read-byte
 
     method read-byte() returns Int
 
-This reads a single byte from the device, without the need to specify a register C<Command>.  This may only be implemented by
-the simplest devices, but it can be used as a short hand for C<read-byte-data> but re-using the previous Command.
+This reads a single byte from the device, without the need to specify
+a register C<Command>.  This may only be implemented by the simplest
+devices, but it can be used as a short hand for C<read-byte-data> but
+re-using the previous Command.
 
 =head2 method write-byte
 
@@ -97,7 +109,8 @@ This reads a single byte from the device register specified by C<$command>.
 
     method write-byte-data(Command $command, Byte $value) returns Int
 
-This writes the single byte C<$value> to the device register C<$command>, returning an int which will be -1 on failure.
+This writes the single byte C<$value> to the device register C<$command>,
+returning an int which will be -1 on failure.
 
 =head2 read-word-data
 
@@ -115,53 +128,58 @@ This writes the 16 bit word C<$value> to the register specified by C<$command> r
 
     method process-call(Command $command, Word $value) returns Int
 
-This writes the 16 bit word C<$value> to the command register C<$command> and then reads it back (presumably the device will
-have changed it.)
+This writes the 16 bit word C<$value> to the command register C<$command>
+and then reads it back (presumably the device will have changed it.)
 
 =head2  method read-block-data
 
     method read-block-data(Command $command) returns Block
 
-This reads the command register C<$value> and returns the C<Block> (an array containing up to 32 unsigned 8 bit integers.)
+This reads the command register C<$value> and returns the C<Block>
+(an array containing up to 32 unsigned 8 bit integers.)
 
 =head2 method write-block-data
 
     method write-block-data(Command $command, Block $block) returns Int
 
-This writes the C<Block> C<$block> (An array of unsigned bytes,) the the command register C<$command> and returns an integer
-to indicate the success of the call.
+This writes the C<Block> C<$block> (An array of unsigned bytes,) the
+the command register C<$command> and returns an integer to indicate the
+success of the call.
 
 =head2 method read-i2c-block-data
 
     method read-i2c-block-data(Command $command, Int $length) returns Block
 
-This is similar to C<read-block-data> but the C<$length> of the number of bytes to be read can be provided, some systems may not
-support anything other than 32.
+This is similar to C<read-block-data> but the C<$length> of the number of
+bytes to be read can be provided, some systems may not support anything
+other than 32.
 
 =head2 method write-i2c-block-data
 
     method write-i2c-block-data(Command $command, Block $block ) returns Int
 
-This is similiar to C<write-block-data> to the extent that the Perl 6 interface is identical.
+This is similiar to C<write-block-data> to the extent that the Perl 6
+interface is identical.
 
 =head2 method block-process-call
 
     method block-process-call(Command $command, Block $block) returns Block
 
-This sends the specified C<Block> (Array of unsigned bytes) to the Command register C<$command> and reads up to 32 bytes
-back which are returned as a C<Block>
+This sends the specified C<Block> (Array of unsigned bytes) to the
+Command register C<$command> and reads up to 32 bytes back which are
+returned as a C<Block>
 
 =end pod
 
-class RPi::Device::SMBus:ver<0.0.3>:auth<github:jonathanstowe>:api<1.0> {
+class RPi::Device::SMBus:ver<0.0.4>:auth<github:jonathanstowe>:api<1.0> {
 
     use NativeHelpers::Array;
 
-    class X::Open is Exception {
+    class X::SMBus::Open is Exception {
         has Str $.message;
     }
 
-    class X::IO is Exception {
+    class X::SMBus::IO is Exception {
         has Str $.message;
     }
 
@@ -193,7 +211,7 @@ class RPi::Device::SMBus:ver<0.0.3>:auth<github:jonathanstowe>:api<1.0> {
         explicitly-manage($file);
         my Int $fd = rpi_dev_smbus_open($file, $address);
         if $fd < 0 {
-            X::Open.new(message => "Error opening the device").throw;
+            X::SMBus::Open.new(message => "Error opening the device").throw;
         }
         $fd;
     }
@@ -256,7 +274,7 @@ class RPi::Device::SMBus:ver<0.0.3>:auth<github:jonathanstowe>:api<1.0> {
         my $len = rpi_dev_smbus_read_block_data(self!fd, $command, $out-buf);
 
         if $len < 0 {
-            X::IO.new(message => "'read_block_data' failed").throw;
+            X::SMBus::IO.new(message => "'read_block_data' failed").throw;
         }
 
         my @array = copy-to-array($out-buf, $len + 1);
@@ -281,7 +299,7 @@ class RPi::Device::SMBus:ver<0.0.3>:auth<github:jonathanstowe>:api<1.0> {
         my $len = rpi_dev_smbus_read_i2c_block_data(self!fd, $command, $length, $out-buf);
 
         if $len < 0 {
-            X::IO.new(message => "'read_i2c_block_data' failed").throw;
+            X::SMBus::IO.new(message => "'read_i2c_block_data' failed").throw;
         }
 
         my @array = copy-to-array($out-buf, $len + 1);
@@ -303,7 +321,7 @@ class RPi::Device::SMBus:ver<0.0.3>:auth<github:jonathanstowe>:api<1.0> {
         my CArray[uint8] $buf = copy-to-carray($block, uint8);
         my $len = rpi_dev_smbus_block_process_call(self!fd, $command, $block.elems, $buf);
         if $len < 0 {
-            X::IO.new(message => "'block_process_call' failed").throw;
+            X::SMBus::IO.new(message => "'block_process_call' failed").throw;
         }
 
         my @array = copy-to-array($buf, $len + 1);
